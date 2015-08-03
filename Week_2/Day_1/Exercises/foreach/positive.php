9dc8c09f40795390c32e830ebe94639dade91842
Mark Nordyke <mnordyke@redventures.net>
2015-08-03 19:42:50 -0400

    updating curriculum

---
+++ Week_2/Day_1/Exercises/foreach/positive.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	</head>
+	<body>
+        <p>
+            <?php
+
+                $intArray = array(0, 1, 5, M_E, -3, 2, -2, -10, 5, 4, M_PI);
+
+                // display all numbers that are less zero
+
+                // code goes here ...
+
+
+
+
+            ?>
+        </p>
+	</body>
+</html>
